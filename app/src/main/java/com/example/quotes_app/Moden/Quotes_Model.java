a21236e55b23686f9873b49195a30b322af05afe
Vasubhalodi011 <vasubhalodi56@gmail.com>
2024-03-06 15:55:31 +0530

    Quotes_App 09-03-2024

---
+++ app/src/main/java/com/example/quotes_app/Moden/Quotes_Model.java
@@ -0,0 +1,25 @@
+package com.example.quotes_app.Moden;
+
+public class Quotes_Model {
+
+    public Integer image;
+    public String Quotes;
+
+    public Quotes_Model(Integer image, String Quotes) {
+        this.image = image;
+        this.Quotes = Quotes;
+
+    }
+
+
+
+    public int getImage() {
+        return image;
+    }
+
+
+    public String getQuotes() {
+        return Quotes;
+    }
+
+}
